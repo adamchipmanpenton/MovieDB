cbc0ebd796988232c711a0f1dec395580fec6a7d
adamchipmanpenton <adamchipmanpenton@gmail.com>
2022-02-01 10:34:19 -0330

    Added bootstrap

--- frontend/src/pages.js
+++ frontend/src/pages.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { useState, useEffect } from 'react';
 import './App.css';
 import {Link, useLocation} from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 
 export function Home({movies, setMovies}) {
@@ -26,6 +32,19 @@ export function Home({movies, setMovies}) {
       }
     return (
         <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="addReview">Movie Reviews</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">See Reviews</Nav.Link>
+                        <Nav.Link href="addReview">Add Review</Nav.Link>
+                        
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <nav>
                 <Link to="addReview">Add a Review</Link>
             </nav>
@@ -108,6 +127,19 @@ export function AddReview({movies, setMovies}) {
      
     return (
         <div className="App">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="addReview">Movie Reviews</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">See Reviews</Nav.Link>
+                        <Nav.Link href="addReview">Add Review</Nav.Link>
+                        
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <nav>
                 <Link to="/">View All Reviews</Link>
             </nav>
